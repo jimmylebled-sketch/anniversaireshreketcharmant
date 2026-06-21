d0089849fea7879271dfdac8e3a31dcdf5c7d6c2
jimmylebled-sketch <jimmylebled@gmail.com>
2026-06-21 11:10:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,96 @@
-const roles = {
-    "quentin yvé": "Nain joyeux",
-    "clara douère": "Sorcière",
-    "pierre sinoquet": "Villageois",
-    "nicolas lamblin": "Garde Far Far Away",
-    "chloé biarnes": "Petit cochon Nouf Nouf",
-    "margot jacques": "Villageoise",
-    "philippine dero": "Sorcière",
-    "titia juif": "Fée Pâquerette",
-    "clara fizet": "Sorcière",
-    "maud reynes": "Fée Flora",
-    "thibault maurice": "Nain Simplet",
-    "hector joulin": "Nain Prof",
-    "malik el hassiny": "Petit cochon Naf Naf",
-    "alhou": "Villageois",
-    "jade pavaut": "Petit cochon Nif Nif",
-    "antoine renault": "Arthur",
-    "yann walbrercq": "Hansel",
-    "louise morisse": "Sorcière",
-    "alizée breton": "Fée Pimprenelle",
-    "gabin nicolas": "Mini Arthur",
-    "eugénie molina": "Blanche Neige",
-    "andréa": "Gretel",
-    "gilles rivières": "Papatissier",
-    "julien nicolas": "Nain Grincheux",
-    "martin mésonier": "Nain Atchoum",
-    "alexis boinet": "Grand Méchant Loup",
-    "guillaume lamarche": "Doris",
-    "ivan vicente": "Souris aveugle",
-    "mathis breton": "Bourreau",
-    "kévin martin": "Pinocchio",
-    "morgane sanchez torres": "Petit Chaperon Rouge",
-    "sébastien vicente": "Souris aveugle",
-    "léa bailleul": "Marraine la Bonne Fée",
-    "morgane breton": "Cendrillon",
-    "bastien stéphan": "Villageois",
-    "kévin arhel": "Lord Farquaad",
-    "bérénice lebreton": "Dragonne",
-    "antoine crevel": "Chat Potté",
-    "hadrien grivart": "Grand Biscuit",
-    "lancelot mésonier": "Robin des Bois",
-    "pauline robin": "Belle au bois dormant",
-    "arnaud mouilleron": "L’Âne",
-    "justinne lannelongue": "Kitty",
-    "antoine duval": "Petit Biscuit",
-    "thomas stachowiak": "Souris aveugle",
-    "amaury mésonier": "Shrek",
-    "astrid barbant": "Raiponce",
-    "pauline bennetot": "Reine Lilliane",
-    "thomas jean": "Roi Harold",
-    "martin lavi": "Tracassin",
-    "jimmy lebled": "Charmant",
-    "axelle breton": "Fiona",
-    "anne-laure folliot": "Sorcière",
-    "curtis gomis": "Miroir magique",
-    "marie le goadec": "Merlin"
-};
+window.addEventListener("DOMContentLoaded", () => {
+    const roles = {
+        "quentin yvé": "Nain joyeux",
+        "clara douère": "Sorcière",
+        "pierre sinoquet": "Villageois",
+        "nicolas lamblin": "Garde Far Far Away",
+        "chloé biarnes": "Petit cochon Nouf Nouf",
+        "margot jacques": "Villageoise",
+        "philippine dero": "Sorcière",
+        "titia juif": "Fée Pâquerette",
+        "clara fizet": "Sorcière",
+        "maud reynes": "Fée Flora",
+        "thibault maurice": "Nain Simplet",
+        "hector joulin": "Nain Prof",
+        "malik el hassiny": "Petit cochon Naf Naf",
+        "alhou": "Villageois",
+        "jade pavaut": "Petit cochon Nif Nif",
+        "antoine renault": "Arthur",
+        "yann walbrercq": "Hansel",
+        "louise morisse": "Sorcière",
+        "alizée breton": "Fée Pimprenelle",
+        "gabin nicolas": "Mini Arthur",
+        "eugénie molina": "Blanche Neige",
+        "andréa": "Gretel",
+        "gilles rivières": "Papatissier",
+        "julien nicolas": "Nain Grincheux",
+        "martin mésonier": "Nain Atchoum",
+        "alexis boinet": "Grand Méchant Loup",
+        "guillaume lamarche": "Doris",
+        "ivan vicente": "Souris aveugle",
+        "mathis breton": "Bourreau",
+        "kévin martin": "Pinocchio",
+        "morgane sanchez torres": "Petit Chaperon Rouge",
+        "sébastien vicente": "Souris aveugle",
+        "léa bailleul": "Marraine la Bonne Fée",
+        "morgane breton": "Cendrillon",
+        "bastien stéphan": "Villageois",
+        "kévin arhel": "Lord Farquaad",
+        "bérénice lebreton": "Dragonne",
+        "antoine crevel": "Chat Potté",
+        "hadrien grivart": "Grand Biscuit",
+        "lancelot mésonier": "Robin des Bois",
+        "pauline robin": "Belle au bois dormant",
+        "arnaud mouilleron": "L’Âne",
+        "justinne lannelongue": "Kitty",
+        "antoine duval": "Petit Biscuit",
+        "thomas stachowiak": "Souris aveugle",
+        "amaury mésonier": "Shrek",
+        "astrid barbant": "Raiponce",
+        "pauline bennetot": "Reine Lilliane",
+        "thomas jean": "Roi Harold",
+        "martin lavi": "Tracassin",
+        "jimmy lebled": "Charmant",
+        "axelle breton": "Fiona",
+        "anne-laure folliot": "Sorcière",
+        "curtis gomis": "Miroir magique",
+        "marie le goadec": "Merlin"
+    };
 
-function normalize(str) {
-    return str
-        .toLowerCase()
-        .trim()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "");
-}
+    function normalize(str) {
+        return str
+            .toLowerCase()
+            .trim()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "");
+    }
 
-/* 🎵 sons */
-const music = document.getElementById("music");
-const click = document.getElementById("click");
-const magic = document.getElementById("magic");
+    /* 🎵 sons */
+    const music = document.getElementById("music");
+    const click = document.getElementById("click");
+    const magic = document.getElementById("magic");
 
-/* 🤠 bouton découverte */
-document.getElementById("discover").onclick = () => {
-    
-    // On baisse le volume et on lance la musique de fond
-    music.volume = 0.3;
-    music.play();
+    /* 🤠 bouton découverte */
+    document.getElementById("discover").onclick = () => {
+        
+        // On baisse le volume et on lance la musique de fond
+        music.volume = 0.3;
+        music.play();
 
-    // On lance le bruit de clic
-    click.play();
-    const name = normalize(document.getElementById("name").value);
-    const role = roles[name];
+        // On lance le bruit de clic
+        click.play();
+        const name = normalize(document.getElementById("name").value);
+        const role = roles[name];
 
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("bookScreen").classList.remove("hidden");
+        document.getElementById("intro").style.display = "none";
+        document.getElementById("bookScreen").classList.remove("hidden");
 
-    setTimeout(() => {
-        magic.play();
+        setTimeout(() => {
+            magic.play();
 
-        document.getElementById("role").innerText =
-            role ? role : "Inconnu du royaume...";
-    }, 800);
-};
+            document.getElementById("role").innerText =
+                role ? role : "Inconnu du royaume...";
+        }, 800);
+    };
+
+});
